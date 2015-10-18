ee0afffbcd26febed754410266cb1d031ab7dfb3
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-10-18 08:56:37 -0600

    --spider and --list now default to top level directory

--- crackmapexec.py
+++ crackmapexec.py
@@ -3078,7 +3078,7 @@ if __name__ == '__main__':
     egroup.add_argument("--wmi", metavar='QUERY', type=str, dest='wmi_query', help='Issues the specified WMI query')
 
     sgroup = parser.add_argument_group("Spidering", "Options for spidering shares")
-    sgroup.add_argument("--spider", metavar='FOLDER', type=str, help='Folder to spider')
+    sgroup.add_argument("--spider", metavar='FOLDER', nargs='?', const='.', type=str, help='Folder to spider')
     sgroup.add_argument("--content", dest='search_content', action='store_true', help='Enable file content searching')
     sgroup.add_argument("--exclude-dirs", type=str, metavar='DIR_LIST', default='', dest='exclude_dirs', help='Directories to exclude from spidering')
     sgroup.add_argument("--pattern", type=str, help='Pattern to search for in folders, filenames and file content (if enabled)')
@@ -3099,7 +3099,7 @@ if __name__ == '__main__':
     xgroup.add_argument("--met-options", nargs=2, metavar=('LHOST', 'LPORT'), dest='met_options', help='Meterpreter options (ignored if not injecting Meterpreter)')
 
     bgroup = parser.add_argument_group("Filesystem Interaction", "Options for interacting with filesystems")
-    bgroup.add_argument("--list", metavar='PATH', help='List contents of a directory')
+    bgroup.add_argument("--list", metavar='PATH', nargs='?', const='.', type=str, help='List contents of a directory')
     bgroup.add_argument("--download", metavar="PATH", help="Download a file from the remote systems")
     bgroup.add_argument("--upload", nargs=2, metavar=('SRC', 'DST'), help="Upload a file to the remote systems")
     bgroup.add_argument("--delete", metavar="PATH", help="Delete a remote file")
