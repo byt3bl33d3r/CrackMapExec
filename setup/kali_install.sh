b1556496196cc7792f1895bdb39a5a6b7d314085
byt3bl33d3r <byt3bl33d3r@gmail.com>
2016-04-16 15:50:21 -0600

    Initial commit for Kali install script

---
+++ setup/kali_install.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+echo -e '\n [*] Installing core deps\n'
+apt-get install libssl-dev libffi-dev python-dev virtualenvwrapper
+
+echo -e '\n [*] Intializing and updating submodules\n'
+git submodule init
+git submodule update --recursive
+
+echo -e '\n [*] Setting up virtualenvwrapper and creating the CME virtualenv\n'
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+echo 'source /usr/share/virtualenvwrapper/virtualenvwrapper.sh' >> ~/.bashrc
+mkvirtualenv CME
+workon CME
+echo 'CME' > ../.venv
+
+echo -e '\n [*] Installing python deps\n'
+pip install -r ../requirements.txt
+
+echo -e '\n [*] Creating the database\n'
+python setup_database.py
