b4559abaac621460733462a9288a5e81151b9441
byt3bl33d3r <byt3bl33d3r@gmail.com>
2016-06-08 21:51:25 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='crackmapexec',
-    version='3.1.0',
+    version='3.1.1',
     description='A swiss army knife for pentesting Windows/Active Directory environments',
     classifiers=[
         'License :: OSI Approved :: BSD License',
