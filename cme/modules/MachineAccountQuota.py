708e8e65ab0257e7dea7a42d1bb51d375b4be9e4
Podalirius <79218792+p0dalirius@users.noreply.github.com>
2021-05-28 10:07:50 +0200

    Added MachineAccountQuota LDAP module
    Retrieves the MachineAccountQuota domain-level attribute

---
+++ cme/modules/MachineAccountQuota.py
@@ -0,0 +1,33 @@
+from impacket.ldap import ldapasn1 as ldapasn1_impacket
+
+
+class CMEModule:
+    '''
+      Module by Shutdown and Podalirius
+
+      Initial module:
+        https://github.com/ShutdownRepo/CrackMapExec-MachineAccountQuota
+
+      Authors:
+        Shutdown: @_nwodtuhs
+        Podalirius: @podalirius_
+    '''
+
+    def options(self, context, module_options):
+        pass
+
+    name = 'MAQ'
+    description = 'Retrieves the MachineAccountQuota domain-level attribute'
+    supported_protocols = ['ldap']
+    opsec_safe = True
+    multiple_hosts = False
+
+    def on_login(self, context, connection):
+        context.log.info('Getting the MachineAccountQuota')
+        searchFilter = '(objectClass=*)'
+        attributes = ['ms-DS-MachineAccountQuota']
+        result = connection.search(searchFilter, attributes, 1)
+        for item in result:
+            if isinstance(item, ldapasn1_impacket.SearchResultEntry) is not True:
+                continue
+            context.log.highlight("MachineAccountQuota: %d" % item['attributes'][0]['vals'][0])
