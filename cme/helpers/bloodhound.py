0cc264f0b2abb082e81e8f6f946909dbbd717186
TNeitzel <qtc_de@outlook.com>
2022-03-03 21:50:55 +0100

    Fix missing BloodHound section bug
    When updating from an older cme version to the new one, bloodhound.py
    raising an exception because it attempts to access the 'BloodHound'
    section from the cme config file, which previously did not exist. This
    commit fixes this issue, by adding a check before performing the action.

--- cme/helpers/bloodhound.py
+++ cme/helpers/bloodhound.py
@@ -1,46 +1,63 @@
 def add_user_bh(user, domain, logger, config):
-    users_owned = []
+
+    if not config.has_option('BloodHound', 'bh_enabled') or config.get('BloodHound', 'bh_enabled') == "False":
+        return
+
+    from neo4j.exceptions import AuthError, ServiceUnavailable
+
+    try:
+        from neo4j.v1 import GraphDatabase
+    except:
+        from neo4j import GraphDatabase
+
     if isinstance(user, str):
-        users_owned.append({'username': user.upper(), 'domain': domain.upper()})
+        users_owned = [{'username': user.upper(), 'domain': domain.upper()}]
     else:
         users_owned = user
-    if config.get('BloodHound', 'bh_enabled') != "False":
-        try:
-            from neo4j.v1 import GraphDatabase
-        except:
-            from neo4j import GraphDatabase
-        from neo4j.exceptions import AuthError, ServiceUnavailable
-        uri = "bolt://{}:{}".format(config.get('BloodHound', 'bh_uri'), config.get('BloodHound', 'bh_port'))
 
-        driver = GraphDatabase.driver(uri, auth=(config.get('BloodHound', 'bh_user'), config.get('BloodHound', 'bh_pass')), encrypted=False)
-        try:
-            with driver.session() as session:
-                with session.begin_transaction() as tx:
-                    for info in users_owned:
-                        if info['username'][-1] == '$':
-                            user_owned = info['username'][:-1] + "." + info['domain']
-                            account_type = 'Computer'
-                        else:
-                            user_owned = info['username'] + "@" + info['domain']
-                            account_type = 'User'
+    bh_uri = config.get('Bloodhound', 'bh_uri')
+    bh_port = config.get('Bloodhound', 'bh_port')
+    bh_user = config.get('Bloodhound', 'bh_user')
+    bh_pass = config.get('Bloodhound', 'bh_pass')
 
-                        result = tx.run(
-                            "MATCH (c:{} {{name:\"{}\"}}) RETURN c".format(account_type, user_owned))
+    uri = "bolt://{}:{}".format(bh_uri, bh_port)
+    driver = GraphDatabase.driver(uri, auth=(bh_user, bh_port), encrypted=False)
 
-                        if result.data()[0]['c'].get('owned') in (False, None):
-                            logger.debug("MATCH (c:{} {{name:\"{}\"}}) SET c.owned=True RETURN c.name AS name".format(account_type, user_owned))
-                            result = tx.run(
-                                "MATCH (c:{} {{name:\"{}\"}}) SET c.owned=True RETURN c.name AS name".format(account_type, user_owned))
-                            logger.highlight("Node {} successfully set as owned in BloodHound".format(user_owned))
-        except AuthError as e:
-            logger.error(
-                "Provided Neo4J credentials ({}:{}) are not valid.".format(config.get('Bloodhound', 'bh_user'), config.get('Bloodhound', 'bh_pass')))
-            return
-        except ServiceUnavailable as e:
-            logger.error("Neo4J does not seem to be available on {}.".format(uri))
-            return
-        except Exception as e:
-            logger.error("Unexpected error with Neo4J")
-            logger.error("Error : ".format(str(e)))
-            return
-        driver.close()
+    try:
+        with driver.session() as session:
+            with session.begin_transaction() as tx:
+
+                for info in users_owned:
+
+                    username = info['username']
+                    domain = info['domain']
+
+                    if username[-1] == '$':
+                        user_owned = "{}.{}".format(username[:-1], domain)
+                        account_type = 'Computer'
+
+                    else:
+                        user_owned = "{}@{}".format(username, domain)
+                        account_type = 'User'
+
+                    result = tx.run( "MATCH (c:{} {{name:\"{}\"}}) RETURN c".format(account_type, user_owned))
+
+                    if result.data()[0]['c'].get('owned') in (False, None):
+                        logger.debug("MATCH (c:{} {{name:\"{}\"}}) SET c.owned=True RETURN c.name AS name".format(account_type, user_owned))
+                        result = tx.run( "MATCH (c:{} {{name:\"{}\"}}) SET c.owned=True RETURN c.name AS name".format(account_type, user_owned))
+                        logger.highlight("Node {} successfully set as owned in BloodHound".format(user_owned))
+
+    except AuthError as e:
+        logger.error("Provided Neo4J credentials ({}:{}) are not valid.".format(bh_user, bh_pass))
+        return
+
+    except ServiceUnavailable as e:
+        logger.error("Neo4J does not seem to be available on {}.".format(uri))
+        return
+
+    except Exception as e:
+        logger.error("Unexpected error with Neo4J")
+        logger.error("Error : ".format(str(e)))
+        return
+
+    driver.close()
