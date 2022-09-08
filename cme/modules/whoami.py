91be977ebbddb170c7e982b5e43c4e8622cde479
mpgn <martial.puygrenier@gmail.com>
2022-09-08 15:04:04 -0400

    update module ldap whoami

--- cme/modules/whoami.py
+++ cme/modules/whoami.py
@@ -20,20 +20,7 @@ class CMEModule:
                 self.username = module_options['USER']
 
     def on_login(self, context, connection):
-
-        # Grab the variables from the CME connection to fill our variables
-        inputUser = connection.domain + '\\' + connection.username
-        inputPassword = connection.password
-        dcTarget = connection.conn.getRemoteHost()
-
-        try:
-            # Connect and bind to the LDAP server
-            ldapServer = Server(dcTarget, use_ssl=False, port=389, get_info=ALL)
-            ldapConn = Connection(ldapServer, user=inputUser, password=inputPassword, authentication=NTLM, auto_bind=True)
-
-            # https://github.com/pycontribs/python3-ldap/blob/master/python3-ldap/ldap3/protocol/rfc4512.py
-            searchBase = ldapServer.info.naming_contexts[0]
-
+            searchBase = connection.ldapConnection._baseDN
             if self.username is None:
                 searchFilter = f'(sAMAccountName={connection.username})'
             else:
@@ -41,52 +28,40 @@ class CMEModule:
                 
             context.log.debug(f'Using naming context: {searchBase} and {searchFilter} as search filter')
 
-            # Confirm login / get username
-            context.log.debug(f'Running LDAP queries as: {ldapConn.extend.standard.who_am_i().replace("u:","")}')
-
             # Get attributes of provided user
-            ldapConn.search(search_base=searchBase,search_filter=searchFilter,
-            attributes=['name','sAmAccountName','description','distinguishedName','pwdLastSet','logonCount','lastLogon','userAccountControl','servicePrincipalName','memberOf'])      
+            r = connection.ldapConnection.search(
+                searchBase=searchBase,
+                searchFilter=searchFilter,
+                attributes=['name','sAmAccountName','description','distinguishedName','pwdLastSet','logonCount','lastLogon','userAccountControl','servicePrincipalName','memberOf'],
+                sizeLimit=999
 
-            for response in ldapConn.response:
-                context.log.highlight(f"Human name: {response['attributes']['name']}")
-                context.log.highlight(f"Username: {response['attributes']['sAmAccountName']}")
-                context.log.highlight(f"Description: {response['attributes']['description']}")
-                context.log.highlight(f"Distinguished name: {response['attributes']['distinguishedName']}")
-                context.log.highlight(f"Password last set: {response['attributes']['pwdLastSet']}")
-                context.log.highlight(f"Logon count: {response['attributes']['logonCount']}")
-
-                if '1601' in str(response['attributes']['lastLogon']):
-                    context.log.highlight(f"Last logon: Never")
-                else:
-                    context.log.highlight(f"Last logon: {response['attributes']['lastLogon']}")
-
-                if response['attributes']['userAccountControl'] == 512:
-                    context.log.highlight(f"Enabled: Yes")
-                    context.log.highlight(f"Password Never Expires: No")
-                if response['attributes']['userAccountControl'] == 514:
-                    context.log.highlight(f"Enabled: No")
-                    context.log.highlight(f"Password Never Expires: No")                    
-                if response['attributes']['userAccountControl'] == 66048:
-                    context.log.highlight(f"Enabled: Yes")
-                    context.log.highlight(f"Password Never Expires: Yes")
-                if response['attributes']['userAccountControl'] == 66050:
-                    context.log.highlight(f"Enabled: No")
-                    context.log.highlight(f"Password Never Expires: Yes")
-
-                if len(response['attributes']['servicePrincipalName']) != 0:
+            )
+            for response in r[0]['attributes']:
+                if 'userAccountControl' in str(response['type']):
+                    if str(response['vals'][0]) == "512":
+                        context.log.highlight(f"Enabled: Yes")
+                        context.log.highlight(f"Password Never Expires: No")
+                    elif str(response['vals'][0]) == "514":
+                        context.log.highlight(f"Enabled: No")
+                        context.log.highlight(f"Password Never Expires: No")
+                    elif str(response['vals'][0]) == "66048":
+                        context.log.highlight(f"Enabled: Yes")
+                        context.log.highlight(f"Password Never Expires: Yes")
+                    elif str(response['vals'][0]) == "66050":
+                        context.log.highlight(f"Enabled: No")
+                        context.log.highlight(f"Password Never Expires: Yes")
+                elif 'lastLogon' in str(response['type']):
+                    if str(response['vals'][0]) == "1601":
+                        context.log.highlight(f"Last logon: Never")
+                    else:
+                        context.log.highlight(f"Last logon: {response['vals'][0]}")
+                elif 'memberOf' in str(response['type']):
+                    for group in response['vals']:
+                        context.log.highlight(f'Member of: {group}')
+                elif 'servicePrincipalName' in str(response['type']):
                     context.log.highlight(f"Service Account Name(s) found - Potentially Kerberoastable user!")
-                    for spn in response['attributes']['servicePrincipalName']:
+                    for spn in response['vals']:
                         context.log.highlight(f"Service Account Name: {spn}")
-                              
-                for group in response['attributes']['memberOf']:
-                    context.log.highlight(f'Member of: {group}')
+                else:
+                    context.log.highlight(response['type'] + ": " +  response['vals'][0])
 
-                # Only want output from first response
-                break
-        
-        except KeyError:
-            context.log.error(f'Username does not exist')
-
-        except Exception as e:
-            context.log.error(f'UNEXPECTED ERROR: {repr(e)}')
