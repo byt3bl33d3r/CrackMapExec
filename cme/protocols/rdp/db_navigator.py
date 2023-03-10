dd79c369810cf1124803ec31ca86a5edd466c5e9
Marshall Hallenbeck <marshall.hallenbeck@gmail.com>
2023-03-09 19:25:18 -0500

    feat(dbs): add help string and confirmation for all database clear_database functions

--- cme/protocols/rdp/db_navigator.py
+++ cme/protocols/rdp/db_navigator.py
@@ -6,4 +6,5 @@ from cme.cmedb import DatabaseNavigator
 
 class navigator(DatabaseNavigator):
     def do_clear_database(self, line):
-        self.db.clear_database()
+        if input("This will destroy all data in the current database, are you SURE you want to run this? (y/n): ") == "y":
+            self.db.clear_database()
