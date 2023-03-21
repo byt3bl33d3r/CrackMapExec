b994efc418e34ea7675e770b45e0419cf5555461
mpgn <martial.puygrenier@gmail.com>
2023-03-21 17:07:18 -0400

    add warning when dumping ntds

--- cme/crackmapexec.py
+++ cme/crackmapexec.py
@@ -302,6 +302,11 @@ def main():
         if ans.lower() not in ['y', 'yes', '']:
             sys.exit(1)
 
+    if (args.ntds and not args.userntds):
+        ans = input(highlight('[!] Dumping the ntds can crash the DC on Windows Server 2019. Use the option --user <user> to dump a specific user safely [Y/n] ', 'red'))
+        if ans.lower() not in ['y', 'yes', '']:
+            sys.exit(1)
+
     try:
         asyncio.run(
             start_threadpool(protocol_object, args, db, targets, jitter)
