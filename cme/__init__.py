34125bc584b35ff40293f056c30f1efbff128963
Marshall Hallenbeck <marshall.hallenbeck@gmail.com>
2023-03-30 00:12:35 -0400

    chore: removed old commented code

--- cme/__init__.py
+++
@@ -1,8 +0,0 @@
-# import sys
-# import os
-# import cme
-
-# thirdparty_modules = os.path.join(os.path.dirname(cme.__file__), 'thirdparty')
-
-# for module in os.listdir(thirdparty_modules):
-#     sys.path.insert(0, os.path.join(thirdparty_modules, module))
