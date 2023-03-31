d3080a3304bddaceb7aa9f02d923ebf10f261257
Marshall Hallenbeck <marshall.hallenbeck@gmail.com>
2023-03-31 11:12:40 -0400

    fix(runasppl): print info log if registry key isnt found, otherwise highlight out to user

--- cme/modules/runasppl.py
+++ cme/modules/runasppl.py
@@ -2,19 +2,23 @@
 # -*- coding: utf-8 -*-
 
 class CMEModule:
-
-    name = 'runasppl'
-    description = "Check if the registry value RunAsPPL is set or not"
-    supported_protocols = ['smb']
-    opsec_safe = True
-    multiple_hosts = True
+    def __init__(self):
+        self.name = "runasppl"
+        self.description = "Check if the registry value RunAsPPL is set or not"
+        self.supported_protocols = ["smb"]
+        self.opsec_safe = True
+        self.multiple_hosts = True
 
     def options(self, context, module_options):
         """
+        Check if the registry value RunAsPPL is set via SMBExec and Powershell
         """
 
     def on_admin_login(self, context, connection):
-        command = 'reg query HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa\ /v RunAsPPL'
-        context.log.info('Executing command')
+        command = "reg query HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa\ /v RunAsPPL"
+        context.log.info("Executing command")
         p = connection.execute(command, True)
-        context.log.highlight(p)
+        if "The system was unable to find the specified registry key or value" in p:
+            context.log.info(f"Unable to find RunAsPPL Registry Key")
+        else:
+            context.log.highlight(p)
