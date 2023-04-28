e6d7e81d7b419200394aa868a4f52f9861c3baba
François REYNAUD <francois.reynaud@orange.com>
2023-04-28 16:45:58 +0200

    Finish simple registry checks and add last update check

--- cme/modules/wcc.py
+++ cme/modules/wcc.py
@@ -8,6 +8,10 @@ from impacket.dcerpc.v5.rrp import DCERPCSessionError
 #from impacket.dcerpc.v5.dtypes import RPC_UNICODE_STRING, DWORD, 
 from impacket.examples.secretsdump import RemoteOperations
 from impacket.system_errors import *
+import operator
+import time
+
+OUTDATED_THRESHOLD = 30
 
 class CMEModule:
 	'''
@@ -35,19 +39,34 @@ class CMEModule:
 
 	def debug(self, msg, *args):
 		if self.verbose:
-			print(f'\x1b[33m{msg}', *args, '\x1b[0m')
+			try:
+				print(f'\x1b[33m{msg}', *args, '\x1b[0m')
+			except BlockingIOError:
+				pass
 
-	def on_login(self, context, connection):
+	def on_admin_login(self, context, connection):
 		self.log = connection.logger
 		self.results.setdefault(connection.host, {'checks':[]})
 		remoteOps = RemoteOperations(smbConnection=connection.conn, doKerberos=False)
 		remoteOps.enableRegistry()
 		dce = remoteOps._RemoteOperations__rrp
-		#pprint(vars(connection))
-		self.check_config(dce, connection.host)
-		#print(self.reg_query_value(dce, 'HKCU\\Volatile Environment', valueName='USERNAME', separator=self.sep))
-		#self.reg_query_value(dce, 'HKCT\\System\\GameConfigStore', valueName=None, separator=self.sep)
+		self.check_config(dce, connection)
+		#self.check_last_successful_update(connection)
 		remoteOps.finish()
+
+	def check_last_successful_update(self, connection):
+		records = connection.wmi(wmi_query='Select TimeGenerated FROM Win32_ReliabilityRecords Where EventIdentifier=19', namespace='root\\cimv2')
+		most_recent_update_date = records[0]['TimeGenerated']['value']
+		most_recent_update_date = most_recent_update_date.split('.')[0]
+		most_recent_update_date = time.strptime(most_recent_update_date, '%Y%m%d%H%M%S')
+		most_recent_update_date = time.mktime(most_recent_update_date)
+		now = time.time()
+		days_since_last_update = (now - most_recent_update_date)//86400
+		if days_since_last_update <= OUTDATED_THRESHOLD:
+			return True, [f'Last update was {days_since_last_update} <= {OUTDATED_THRESHOLD} days ago']
+		else:
+			return False, [f'Last update was {days_since_last_update} > {OUTDATED_THRESHOLD} days ago']
+
 
 	def add_result(self, host, result):
 		self.results[host]['checks'].append({
@@ -57,11 +76,12 @@ class CMEModule:
 			"Reasons":result.reasons
 		})
 
-	def check_config(self, dce, host):
+	def check_config(self, dce, connection):
+		host = connection.host
 		module = self
 		class ConfigCheck:
 			"""
-			Class for doing the simple checks that only require checking registry values
+			Class for performing the checks and holding the results
 			"""
 			def __init__(self, name, description=""):
 				self.name = name
@@ -69,12 +89,48 @@ class CMEModule:
 				self.ok = False
 				self.reasons = []
 
-			def check(self, *specs):
+			def check(self, *specs, op=operator.eq):
+				"""
+				Perform checks that only require to compare values in the registry with expected values, according to the specs
+				a spec may be either a 3-tuple: (key name, value name, expected value), or a 4-tuple (key name, value name, expected value, operation), where operation is a function that implements a comparison operator
+				"""
 				self.ok = True
-				for (key, value_name, expected_values) in specs:
-					module.debug(f'Checking if {key}\\{value_name} == {expected_values}')
+
+				for spec in specs:
+					if len(spec) == 3:
+						(key, value_name, expected_value) = spec
+						op = operator.eq
+					elif len(spec) == 4:
+						(key, value_name, expected_value, op) = spec
+					if op == operator.eq:
+						opstring = '{left} == {right}'
+						nopstring = '{left} != {right}'
+					elif op == operator.contains:
+						opstring = '{left} in {right}'
+						nopstring = '{left} not in {right}'
+					elif op == operator.gt:
+						opstring = '{left} > {right}'
+						nopstring = '{left} <= {right}'
+					elif op == operator.ge:
+						opstring = '{left} >= {right}'
+						nopstring = '{left} < {right}'
+					elif op == operator.lt:
+						opstring = '{left} < {right}'
+						nopstring = '{left} >= {right}'
+					elif op == operator.le:
+						opstring = '{left} <= {right}'
+						nopstring = '{left} > {right}'
+					elif op == operator.ne:
+						opstring = '{left} != {right}'
+						nopstring = '{left} == {right}'
+					else:
+						opstring = f'{op.__name__}({{left}}, {{right}}) == True '
+						nopstring = f'{op.__name__}({{left}}, {{right}}) == True '
+
+					module.debug('Checking if {0}'.format(opstring.format(left=f'{key}\\{value_name}', right=f'{expected_value}')))
 					value = module.reg_query_value(dce, key, value_name)
 					module.debug(f'Got value {value}')
+
 					if type(value) == DCERPCSessionError:
 						self.ok = False
 						if value.error_code in (ERROR_NO_MORE_ITEMS, ERROR_FILE_NOT_FOUND):
@@ -83,20 +139,20 @@ class CMEModule:
 							self.reasons.append(f'{value_name}: Value not found')
 						continue
 
-					try:
-						iter(expected_values)
-					except TypeError:
-						if value == expected_values:
-							self.reasons.append(f'{value_name} == {value}')
-						else:
-							self.reasons.append(f'{value_name} ({value}) != {expected_values}')
-							self.ok = False
+					if op(value, expected_value):
+						self.reasons.append(opstring.format(left=f'{key}\\{value_name} ({value})', right=expected_value))
 					else:
-						if value in expected_values:
-							self.reasons.append(f'{value_name} == {value}')
-						else:
-							self.reasons.append(f'{value_name} not in {expected_values}')
-							self.ok = False
+						self.reasons.append(nopstring.format(left=f'{key}\\{value_name} ({value})', right=expected_value))
+						self.ok = False
+
+				return self
+
+			def wrap_check(self, check_function, *args, **kwargs):
+				"""
+				Execute the given check function with the given arguments, and update internal attributes according to the results.
+				The check function MUST return a boolean and a list of strings
+				"""
+				self.ok, self.reasons = check_function(*args, **kwargs)
 				return self
 
 			def log(self):
@@ -110,18 +166,16 @@ class CMEModule:
 		# TODO: check_administrator_name
 		# TODO: check_print_spooler_service
 		# TODO: check_wsus
-		# TODO: check_lsa_cache
 		# TODO: check_applocker
-		# TODO: check_rdp_expiration_time
 		# TODO: check_powershell_v2_availability
-		# TODO: check_ntlmv1
 		# TODO: check_nbtns
+		# TODO: check_smb_encryption
 		# TODO: check_bitlockerconf
 		# TODO: check_guest_account
-		# TODO: check_session_lock
-		# TODO: check_legal_notice
+		# TODO: check_execution_policy
 
 		for result in (
+			ConfigCheck('Last successful update', 'Checks how old is the last successful update').wrap_check(self.check_last_successful_update, connection),
 			ConfigCheck('UAC configuration', 'Checks if UAC configuration is secure').check((
 					'HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System',
 					'EnableLUA', 1
@@ -142,12 +196,25 @@ class CMEModule:
 			),
 			ConfigCheck('IPv6 preference', 'Checks if IPv6 is preferred over IPv4').check((
 					'HKLM\\SYSTEM\\CurrentControlSet\\Services\\Tcpip6\\Parameters',
-					'DisabledComponents', (32, 255)
+					'DisabledComponents', (32, 255), in_
 				)
 			),
 			ConfigCheck('WDigest authentication', 'Checks if WDigest authentication is disabled').check((
 					'HKLM\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\WDigest',
 					'UseLogonCredential', 0
+				)
+			),
+			ConfigCheck('LSA cache', 'Checks how many logons are kept in the LSA cache').check((
+					'HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon',
+					'CachedLogonsCount', 2, le
+				)
+			),
+			ConfigCheck('RDP expiration time', 'Checks RDP session timeout').check((
+					'HKLM\\SOFTWARE\\Policies\\Microsoft\\Windows NT\\Terminal Services',
+					'MaxDisconnectionTime', 0, operator.gt
+				),(
+					'HKCU\\SOFTWARE\\Policies\\Microsoft\\Windows NT\\Terminal Services',
+					'MaxDisconnectionTime', 0, operator.gt
 				)
 			),
 			ConfigCheck('CredentialGuard', 'Checks if CredentialGuard is enabled').check((
@@ -166,6 +233,11 @@ class CMEModule:
 			ConfigCheck('PEAP certificate validation', 'Checks if PEAP certificate validation is enabled').check((
 					'HKLM\\SYSTEM\\CurrentControlSet\\Services\\Rasman\\PPP\\EAP\\13',
 					'ValidateServerCert', 1
+				)
+			),
+			ConfigCheck('NTLMv1', 'Checks if NTLMv1 authentication is disabled').check((
+					'HKLM\\SYSTEM\\CurrentControlSet\\Control\\Lsa',
+					'LmCompatibilityLevel', 5, operator.ge
 				)
 			),
 			ConfigCheck('mDNS', 'Checks if mDNS is disabled').check((
@@ -202,6 +274,19 @@ class CMEModule:
 				),(
 					'HKLM\\SYSTEM\\CurrentControlSet\\Control\\LSA',
 					'RestrictedAdminMode', 1
+				)
+			),
+			ConfigCheck('Automatic session lock', 'Checks if the session is automatically locked on after a period of inactivity').check((
+					'HKCU\\Control Panel\\Desktop',
+					'ScreenSaverIsSecure', 1
+				),(
+					'HKCU\\Control Panel\\Desktop',
+					'ScreenSaveTimeOut', 300, le
+				)
+			),
+			ConfigCheck('Legal notice banner', 'Checks if there is a legal notice banner set').check((
+					'HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System',
+					'legalnoticecaption', "", operator.ne
 				)
 			)
 		):
@@ -287,3 +372,8 @@ class CMEModule:
 		return data
 
 
+def le(reg_sz_string, number):
+	return int(reg_sz_string[:-1]) <= number
+
+def in_(obj, seq):
+	return obj in seq
