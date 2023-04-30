12607511944c8f360cea3e8d270a3d6d5a515ac1
Marshall Hallenbeck <marshall.hallenbeck@gmail.com>
2023-04-30 17:24:18 -0400

    fix(mssql): change how mssql returns results, bypassing impacket/tds jank logging and fix handlekatz/nanodump modules

--- cme/protocols/mssql.py
+++ cme/protocols/mssql.py
@@ -63,7 +63,7 @@ class mssql(connection):
             self.enum_host_info()
             self.print_host_info()
             self.login()
-            if hasattr(self.args, 'module') and self.args.module:
+            if hasattr(self.args, "module") and self.args.module:
                 self.call_modules()
             else:
                 self.call_cmd_args()
@@ -71,10 +71,10 @@ class mssql(connection):
     def proto_logger(self):
         self.logger = CMEAdapter(
             extra={
-                'protocol': 'MSSQL',
-                'host': self.host,
-                'port': self.args.port,
-                'hostname': 'None'
+                "protocol": "MSSQL",
+                "host": self.host,
+                "port": self.args.port,
+                "hostname": "None"
             }
         )
 
@@ -82,7 +82,7 @@ class mssql(connection):
         # this try pass breaks module http server, more info https://github.com/byt3bl33d3r/CrackMapExec/issues/363
         try:
             # Probably a better way of doing this, grab our IP from the socket
-            self.local_ip = str(self.conn.socket).split()[2].split('=')[1].split(':')[0]
+            self.local_ip = str(self.conn.socket).split()[2].split("=")[1].split(":")[0]
         except:
             pass
 
@@ -92,7 +92,7 @@ class mssql(connection):
             try:
                 smb_conn = SMBConnection(self.host, self.host, None)
                 try:
-                    smb_conn.login('', '')
+                    smb_conn.login("", "")
                 except SessionError as e:
                     if "STATUS_ACCESS_DENIED" in e.getErrorString():
                         pass
@@ -100,7 +100,7 @@ class mssql(connection):
                 self.domain = smb_conn.getServerDNSDomainName()
                 self.hostname = smb_conn.getServerName()
                 self.server_os = smb_conn.getServerOS()
-                self.logger.extra['hostname'] = self.hostname
+                self.logger.extra["hostname"] = self.hostname
 
                 try:
                     smb_conn.logoff()
@@ -136,7 +136,7 @@ class mssql(connection):
 
     def create_conn_obj(self):
         try:
-            self.conn = tds.MSSQL(self.host, self.args.port, rowsPrinter=self.logger)
+            self.conn = tds.MSSQL(self.host, self.args.port)
             self.conn.connect()
         except socket.error:
             return False
@@ -147,19 +147,18 @@ class mssql(connection):
         try:
             results = self.conn.sql_query("SELECT IS_SRVROLEMEMBER('sysadmin')")
             is_admin = int(results[0][""])
-
-            if is_admin:
-                self.admin_privs = True
-                self.logger.debug(f"User is admin")
-            else:
-                return False
         except Exception as e:
-            self.logger.fail(f"Error calling check_if_admin(): {e}")
+            self.logger.fail(f"Error querying for sysadmin role: {e}")
             return False
 
+        if is_admin:
+            self.admin_privs = True
+            self.logger.debug(f"User is admin")
+        else:
+            return False
         return True
 
-    def kerberos_login(self, domain, username, password='', ntlm_hash='', aesKey='', kdcHost='', useCache=False):
+    def kerberos_login(self, domain, username, password="", ntlm_hash="", aesKey="", kdcHost="", useCache=False):
         try:
             self.conn.disconnect()
         except:
@@ -167,15 +166,15 @@ class mssql(connection):
         self.create_conn_obj()
         logging.getLogger("impacket").disabled = True
 
-        nthash = ''
+        nthash = ""
         hashes = None
-        if ntlm_hash != '':
-            if ntlm_hash.find(':') != -1:
+        if ntlm_hash != "":
+            if ntlm_hash.find(":") != -1:
                 hashes = ntlm_hash
-                nthash = ntlm_hash.split(':')[1]
+                nthash = ntlm_hash.split(":")[1]
             else:
                 # only nt hash
-                hashes = ':%s' % ntlm_hash
+                hashes = f":{ntlm_hash}"
                 nthash = ntlm_hash
 
         if not all('' == s for s in [self.nthash, password, aesKey]):
@@ -190,7 +189,7 @@ class mssql(connection):
 
             self.password = password
             if username == '' and useCache:
-                ccache = CCache.loadFile(os.getenv('KRB5CCNAME'))
+                ccache = CCache.loadFile(os.getenv("KRB5CCNAME"))
                 principal = ccache.principal.toPrincipal()
                 self.username = principal.components[0]
                 username = principal.components[0]
@@ -285,9 +284,9 @@ class mssql(connection):
             res = self.conn.login(
                 None,
                 username,
-                '',
+                "",
                 domain,
-                ':' + nthash if not lmhash else ntlm_hash,
+                ":" + nthash if not lmhash else ntlm_hash,
                 not self.args.local_auth
             )
             if res is not True:
@@ -298,10 +297,10 @@ class mssql(connection):
             self.username = username
             self.domain = domain
             self.check_if_admin()
-            self.db.add_credential('hash', domain, username, ntlm_hash)
+            self.db.add_credential("hash", domain, username, ntlm_hash)
 
             if self.admin_privs:
-                self.db.add_admin_user('hash', domain, username, ntlm_hash, self.host)
+                self.db.add_admin_user("hash", domain, username, ntlm_hash, self.host)
 
             out = u"{}\\{} {} {}".format(
                 domain,
@@ -323,7 +322,8 @@ class mssql(connection):
             return False
 
     def mssql_query(self):
-        self.conn.sql_query(self.args.mssql_query)
+        result = self.conn.sql_query(self.args.mssql_query)
+        self.logger.debug(f"Result: {result}")
         self.conn.printRows()
         for line in StringIO(self.conn._MSSQL__rowsPrinter.getMessage()).readlines():
             if line.strip() != '':
@@ -334,27 +334,31 @@ class mssql(connection):
     def execute(self, payload=None, get_output=False, methods=None):
         if not payload and self.args.execute:
             payload = self.args.execute
-            if not self.args.no_output: get_output = True
+            if not self.args.no_output:
+                get_output = True
 
         self.logger.info(f"Command to execute:\n{payload}")
-        exec_method = MSSQLEXEC(self.conn)
-        raw_output = exec_method.execute(payload, get_output)
+        try:
+            exec_method = MSSQLEXEC(self.conn)
+            raw_output = exec_method.execute(payload, get_output)
+        except Exception as e:
+            self.logger.exception(e)
+            return None
+
         self.logger.info("Executed command via mssqlexec")
 
         if hasattr(self, "server"):
             self.server.track_host(self.host)
 
-        output = f"{raw_output}"
-
         if self.args.execute or self.args.ps_execute:
-            #self.logger.success('Executed command {}'.format('via {}'.format(self.args.exec_method) if self.args.exec_method else ''))
+            # self.logger.success('Executed command {}'.format('via {}'.format(self.args.exec_method) if self.args.exec_method else ''))
             self.logger.success("Executed command via mssqlexec")
-            buf = StringIO(output).readlines()
+            buf = StringIO(raw_output).readlines()
             for line in buf:
-                if line.strip() != '':
+                if line.strip() != "":
                     self.logger.highlight(line.strip())
 
-        return output
+        return raw_output
 
     @requires_admin
     def ps_execute(self, payload=None, get_output=False, methods=None, force_ps32=False, dont_obfs=True):
@@ -362,7 +366,7 @@ class mssql(connection):
             payload = self.args.ps_execute
             if not self.args.no_output: get_output = True
 
-        # We're disabling PS obfuscation by default as it breaks the MSSQLEXEC execution method (probably an escaping issue)
+        # We're disabling PS obfuscation by default as it breaks the MSSQLEXEC execution method
         ps_command = create_ps_command(payload, force_ps32=force_ps32, dont_obfs=dont_obfs)
         return self.execute(ps_command, get_output)
 
@@ -392,49 +396,47 @@ class mssql(connection):
         except Exception as e:
             self.logger.fail(f"Error reading file {self.args.get_file[0]}: {e}")
 
+    # We hook these functions in the tds library to use CME's logger instead of printing the output to stdout
+    # The whole tds library in impacket needs a good overhaul to preserve my sanity
+    def printRepliesCME(self):
+        for keys in self.replies.keys():
+            for i, key in enumerate(self.replies[keys]):
+                if key["TokenType"] == TDS_ERROR_TOKEN:
+                    error = f"ERROR({key['ServerName'].decode('utf-16le')}): Line {key['LineNumber']:d}: {key['MsgText'].decode('utf-16le')}"
+                    self.lastError = SQLErrorException(
+                        f"ERROR: Line {key['LineNumber']:d}: {key['MsgText'].decode('utf-16le')}"
+                    )
+                    self._MSSQL__rowsPrinter.error(error)
 
-# We hook these functions in the tds library to use CME's logger instead of printing the output to stdout
-# The whole tds library in impacket needs a good overhaul to preserve my sanity
-def printRepliesCME(self):
-    for keys in self.replies.keys():
-        for i, key in enumerate(self.replies[keys]):
-            if key["TokenType"] == TDS_ERROR_TOKEN:
-                error = f"ERROR({key['ServerName'].decode('utf-16le')}): Line {key['LineNumber']:d}: {key['MsgText'].decode('utf-16le')}"
-                self.lastError = SQLErrorException(
-                    f"ERROR: Line {key['LineNumber']:d}: {key['MsgText'].decode('utf-16le')}"
-                )
-                self._MSSQL__rowsPrinter.error(error)
-
-            elif key["TokenType"] == TDS_INFO_TOKEN:
-                self._MSSQL__rowsPrinter.info(
-                    f"INFO({key['ServerName'].decode('utf-16le')}): Line {key['LineNumber']:d}: {key['MsgText'].decode('utf-16le')}"
-                )
-
-            elif key["TokenType"] == TDS_LOGINACK_TOKEN:
-                self._MSSQL__rowsPrinter.info(
-                    f"ACK: Result: {key['Interface']} - {key['ProgName'].decode('utf-16le')} ({key['MajorVer']:d}{key['MinorVer']:d} {key['BuildNumHi']:d}{key['BuildNumLow']:d}) "
-                )
-
-            elif key["TokenType"] == TDS_ENVCHANGE_TOKEN:
-                if key["Type"] in (TDS_ENVCHANGE_DATABASE, TDS_ENVCHANGE_LANGUAGE, TDS_ENVCHANGE_CHARSET, TDS_ENVCHANGE_PACKETSIZE):
-                    record = TDS_ENVCHANGE_VARCHAR(key["Data"])
-                    if record["OldValue"] == "":
-                        record["OldValue"] = "None".encode('utf-16le')
-                    elif record["NewValue"] == '':
-                        record["NewValue"] = "None".encode('utf-16le')
-                    if key["Type"] == TDS_ENVCHANGE_DATABASE:
-                        _type = "DATABASE"
-                    elif key["Type"] == TDS_ENVCHANGE_LANGUAGE:
-                        _type = "LANGUAGE"
-                    elif key["Type"] == TDS_ENVCHANGE_CHARSET:
-                        _type = "CHARSET"
-                    elif key["Type"] == TDS_ENVCHANGE_PACKETSIZE:
-                        _type = "PACKETSIZE"
-                    else:
-                        _type = f"{key['Type']:d}"
+                elif key["TokenType"] == TDS_INFO_TOKEN:
                     self._MSSQL__rowsPrinter.info(
-                        f"ENVCHANGE({_type}): Old Value: {record['OldValue'].decode('utf-16le')}, New Value: {record['NewValue'].decode('utf-16le')}"
+                        f"INFO({key['ServerName'].decode('utf-16le')}): Line {key['LineNumber']:d}: {key['MsgText'].decode('utf-16le')}"
                     )
 
+                elif key["TokenType"] == TDS_LOGINACK_TOKEN:
+                    self._MSSQL__rowsPrinter.info(
+                        f"ACK: Result: {key['Interface']} - {key['ProgName'].decode('utf-16le')} ({key['MajorVer']:d}{key['MinorVer']:d} {key['BuildNumHi']:d}{key['BuildNumLow']:d}) "
+                    )
 
-tds.MSSQL.printReplies = printRepliesCME
+                elif key["TokenType"] == TDS_ENVCHANGE_TOKEN:
+                    if key["Type"] in (TDS_ENVCHANGE_DATABASE, TDS_ENVCHANGE_LANGUAGE, TDS_ENVCHANGE_CHARSET, TDS_ENVCHANGE_PACKETSIZE):
+                        record = TDS_ENVCHANGE_VARCHAR(key["Data"])
+                        if record["OldValue"] == "":
+                            record["OldValue"] = "None".encode('utf-16le')
+                        elif record["NewValue"] == '':
+                            record["NewValue"] = "None".encode('utf-16le')
+                        if key["Type"] == TDS_ENVCHANGE_DATABASE:
+                            _type = "DATABASE"
+                        elif key["Type"] == TDS_ENVCHANGE_LANGUAGE:
+                            _type = "LANGUAGE"
+                        elif key["Type"] == TDS_ENVCHANGE_CHARSET:
+                            _type = "CHARSET"
+                        elif key["Type"] == TDS_ENVCHANGE_PACKETSIZE:
+                            _type = "PACKETSIZE"
+                        else:
+                            _type = f"{key['Type']:d}"
+                        self._MSSQL__rowsPrinter.info(
+                            f"ENVCHANGE({_type}): Old Value: {record['OldValue'].decode('utf-16le')}, New Value: {record['NewValue'].decode('utf-16le')}"
+                        )
+
+    tds.MSSQL.printReplies = printRepliesCME
