d0fabf38d0a13785c39b934e0163094dbae4f2b5
Marshall Hallenbeck <marshall.hallenbeck@gmail.com>
2023-05-31 10:21:16 -0400

    fix(ftp): remove redundant argument options

--- cme/protocols/ftp/proto_args.py
+++ cme/protocols/ftp/proto_args.py
@@ -1,16 +1,6 @@
 def proto_args(parser, std_parser, module_parser):
     ftp_parser = parser.add_parser("ftp", help="own stuff using FTP", parents=[std_parser, module_parser])
-    ftp_parser.add_argument(
-        "--no-bruteforce",
-        action="store_true",
-        help="No spray when using file for username and password (user1 => password1, user2 => password2",
-    )
     ftp_parser.add_argument("--port", type=int, default=21, help="FTP port (default: 21)")
-    ftp_parser.add_argument(
-        "--continue-on-success",
-        action="store_true",
-        help="continues authentication attempts even after successes",
-    )
 
     cgroup = ftp_parser.add_argument_group("FTP Access", "Options for enumerating your access")
     cgroup.add_argument("--ls", action="store_true", help="List files in the directory")
