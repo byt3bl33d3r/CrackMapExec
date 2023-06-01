a2a728df7fb22670afb358c7ebfe4ae9a4d8de55
Cyber Celt <33097451+Cyb3rC3lt@users.noreply.github.com>
2023-06-01 12:16:44 +0100

    Update group_members.py
    Refactored the code and made it easier to read

--- cme/modules/group_members.py
+++ cme/modules/group_members.py
@@ -17,6 +17,7 @@ class CMEModule:
     opsec_safe = True
     multiple_hosts = False
     primaryGroupID = ''
+    answers = []
 
     def options(self, context, module_options):
         '''
@@ -43,6 +44,7 @@ class CMEModule:
         searchResult = doSearch(self, context, connection, searchFilter, attribute)
         #If no SID for the Group is returned exit the program
         if searchResult is None:
+            context.log.success('Unable to find any members of the "' + self.GROUP + '" group')
             return True
 
         # Convert the binary SID to a primaryGroupID string to be used further
@@ -52,20 +54,17 @@ class CMEModule:
         #Look up the groups DN
         searchFilter = "(&(objectCategory=group)(cn=" + self.GROUP + "))"
         attribute = "distinguishedName"
-
         distinguishedName = (doSearch(self, context, connection, searchFilter, attribute)).decode("utf-8")
 
-        # If the primaryGroupID relates to Domain Users or Guests use this unique filter
-        if self.primaryGroupID == "513" or self.primaryGroupID == "514":
-            searchFilter = "(|(memberOf="+distinguishedName+")(primaryGroupID="+self.primaryGroupID+"))"
-            attribute = "sAMAccountName"
-            searchResult = doSearch(self, context, connection, searchFilter, attribute)
+        # Carry out the search
+        searchFilter = "(|(memberOf="+distinguishedName+")(primaryGroupID="+self.primaryGroupID+"))"
+        attribute = "sAMAccountName"
+        searchResult = doSearch(self, context, connection, searchFilter, attribute)
 
-        # Else If the primaryGroupID belongs to another group use the normal lookup
-        else:
-            searchFilter = "(&(|(objectCategory=user)(objectCategory=group))(memberOf="+distinguishedName+"))"
-            attribute = "sAMAccountName"
-            searchResult = doSearch(self, context, connection, searchFilter, attribute)
+        if len(self.answers) > 0:
+            context.log.success('Found the following members of the ' + self.GROUP + ' group:')
+            for answer in self.answers:
+                context.log.highlight(u'{}'.format(answer[0]))
 
 # Carry out an LDAP search for the Group with the supplied Group name
 def doSearch(self,context, connection,searchFilter,attributeName):
@@ -74,7 +73,6 @@ def doSearch(self,context, connection,searchFilter,attributeName):
         resp = connection.ldapConnection.search(searchFilter=searchFilter,
                                                 attributes=[attributeName],
                                                 sizeLimit=0)
-        answers = []
         context.log.debug('Total no. of records returned %d' % len(resp))
         for item in resp:
             if isinstance(item, ldapasn1_impacket.SearchResultEntry) is not True:
@@ -92,24 +90,11 @@ def doSearch(self,context, connection,searchFilter,attributeName):
                         else:
                              attributeValue = str(attribute['vals'][0])
                     if attributeValue is not None:
-                        answers.append([attributeValue])
+                        self.answers.append([attributeValue])
             except Exception as e:
                 context.log.debug("Exception:", exc_info=True)
                 context.log.debug('Skipping item, cannot process due to error %s' % str(e))
                 pass
-        if len(answers) > 0:
-            context.log.success('Found the following members of the ' + self.GROUP + ' group:')
-            for answer in answers:
-                context.log.highlight(u'{}'.format(answer[0]))
-        else:
-            # If no results at this stage and the group name is correct, check for machine accounts
-            if self.primaryGroupID != '':
-             searchFilter = "(primaryGroupID="+self.primaryGroupID+")"
-             attribute = "dNSHostName"
-             doSearch(self, context, connection, searchFilter, attribute)
-            else:
-                context.log.success('Unable to find any members of the "' + self.GROUP + '" group')
-
     except Exception as e:
         context.log.debug("Exception:", e)
         return False
