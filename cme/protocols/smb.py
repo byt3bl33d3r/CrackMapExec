2791ba9ff34e4888b6bd3b55e56550800b3e703b
mpgn <martial.puygrenier@gmail.com>
2023-06-15 03:55:24 -0400

    fix share filter

--- cme/protocols/smb.py
+++ cme/protocols/smb.py
@@ -882,8 +882,7 @@ class smb(connection):
             name = share["name"]
             remark = share["remark"]
             perms = share["access"]
-
-            if self.args.filter_shares and self.args.filter_shares != perms:
+            if self.args.filter_shares and not any(x in perms for x in self.args.filter_shares):
                 continue
             self.logger.highlight(f"{name:<15} {','.join(perms):<15} {remark}")
         return permissions
