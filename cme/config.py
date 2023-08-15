345f2d09fc7d0e48122963345fec58ec6c5bde24
Alex <61382599+NeffIsBack@users.noreply.github.com>
2023-08-15 22:07:01 +0200

    Update config file if options are missing

--- cme/config.py
+++ cme/config.py
@@ -1,8 +1,13 @@
 # coding=utf-8
 import os
+from os.path import join as path_join
 import configparser
-from cme.paths import CME_PATH
+from cme.paths import CME_PATH, DATA_PATH
 from cme.first_run import first_run_setup
+from cme.logger import cme_logger
+
+cme_default_config = configparser.ConfigParser()
+cme_default_config.read(path_join(DATA_PATH, "cme.conf"))
 
 cme_config = configparser.ConfigParser()
 cme_config.read(os.path.join(CME_PATH, "cme.conf"))
@@ -11,6 +16,12 @@ if "CME" not in cme_config.sections():
     first_run_setup()
     cme_config.read(os.path.join(CME_PATH, "cme.conf"))
 
+for option in cme_default_config.options("CME"):
+    if option not in cme_config.options("CME"):
+        cme_logger.info("Adding missing option '{}' to cme.conf".format(option))
+        cme_config.set("CME", option, cme_default_config.get("CME", option))
+
+# These options have to exist in the default config file!!
 cme_workspace = cme_config.get("CME", "workspace", fallback="default")
 config_log = cme_config.getboolean("CME", "log_mode", fallback=False)
 ignore_opsec = cme_config.getboolean("CME", "ignore_opsec", fallback=False)
