6bfe18ad6d1aaa6d6e441b48344af1440c988358
Alex <61382599+NeffIsBack@users.noreply.github.com>
2023-08-18 11:58:24 +0200

    Add logging best practices to example module

--- cme/modules/example_module.py
+++ cme/modules/example_module.py
@@ -10,7 +10,7 @@ class CMEModule:
 
     name = "example module"
     description = "I do something"
-    supported_protocols = []
+    supported_protocols = [] # Example: ['smb', 'mssql']
     opsec_safe = True  # Does the module touch disk?
     multiple_hosts = True  # Does it make sense to run this module on multiple hosts at a time?
 
@@ -28,7 +28,23 @@ class CMEModule:
         """Concurrent.
         Required if on_admin_login is not present. This gets called on each authenticated connection
         """
-        pass
+        # Logging best practice
+        # Mostly you should use these functions to display information to the user
+        context.log.display("I'm doing something")     # Use this for every normal message ([*] I'm doing something)
+        context.log.success("I'm doing something")     # Use this for when something succeeds ([+] I'm doing something)
+        context.log.fail("I'm doing something")        # Use this for when something fails ([-] I'm doing something), for example a remote registry entry is missing which is needed to proceed
+        context.log.highlight("I'm doing something")   # Use this for when something is important and should be highlighted, printing credentials for example
+
+        # These are for debugging purposes
+        context.log.info("I'm doing something")        # This will only be displayed if the user has specified the --verbose flag, so add additional info that might be useful
+        context.log.debug("I'm doing something")       # This will only be displayed if the user has specified the --debug flag, so add info that you would might need for debugging errors
+
+        # These are for more critical error handling
+        context.log.error("I'm doing something")       # This will not be printed in the module context and should only be used for critical errors (e.g. a required python file is missing)
+        try:
+            raise Exception("Exception that might occure")
+        except Exception as e:
+            context.log.exception(f"Exception occured: {e}")   # This will display an exception traceback screen after an exception was raised and should only be used for critical errors
 
     def on_admin_login(self, context, connection):
         """Concurrent.
