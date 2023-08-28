21b3ea6315dc74f9344a030a02ce467caa966a9f
XiaoliChan <2209553467@qq.com>
2023-08-28 18:06:06 +0800

    [wmi] self review: add ipv6 error message
    Signed-off-by: XiaoliChan <2209553467@qq.com>

--- cme/protocols/wmi.py
+++ cme/protocols/wmi.py
@@ -185,8 +185,13 @@ class wmi(connection):
             if not str(e).find("access_denied") > 0:
                 self.logger.fail(str(e))
         else:
-            if not flag:
-                self.logger.fail(f'Check admin error: dcom initialization failed with stringbinding: "{self.stringBinding}", please try "--rpc-timeout" option. (probably is admin)')
+            if not flag or not self.stringBinding:
+                error_msg = f'dcom initialization failed with stringbinding: "{self.stringBinding}", please try "--rpc-timeout" option. (probably is admin)'
+                
+                if not self.stringBinding:
+                    error_msg = "Check admin error: dcom initialization failed: can't get target stringbinding, maybe cause by IPv6 or any other issues, please check your target again"
+                
+                self.logger.fail(f'Check admin error: {error_msg}')
                 try:
                     dcom.disconnect()
                 except:
