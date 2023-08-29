c8cf68ef40a87ef680890a06c57d97d3ff95996a
Shad0wC0ntr0ller <90877534+Shad0wC0ntr0ller@users.noreply.github.com>
2023-08-29 11:38:07 -0400

    Update trust.py

--- cme/modules/trust.py
+++ cme/modules/trust.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 class CMEModule:
     '''
         Extract all Trust Relationships, Trusting Direction, and Trust Transitivity
@@ -20,20 +19,12 @@ class CMEModule:
         domain_dn = ','.join(['DC=' + dc for dc in connection.domain.split('.')])
         search_filter = '(&(objectClass=trustedDomain))'
         attributes = ['flatName', 'trustPartner', 'trustDirection', 'trustAttributes'] 
-        try:
-            context.log.debug('Search Filter=%s' % search_filter)
-            resp = connection.ldapConnection.search(searchBase=domain_dn, searchFilter=search_filter, attributes=attributes, sizeLimit=0)
-        except ldap_impacket.LDAPSearchError as e:
-            if e.getErrorString().find('sizeLimitExceeded') >= 0:
-                context.log.debug('sizeLimitExceeded exception caught, giving up and processing the data received')
-                resp = e.getAnswers()
-                pass
-            else:
-                context.log.debug(e)
-                return False
+
+        context.log.debug(f'Search Filter={search_filter}')
+        resp = connection.ldapConnection.search(searchBase=domain_dn, searchFilter=search_filter, attributes=attributes, sizeLimit=0)
 
         trusts = []
-        context.log.debug('Total of records returned %d' % len(resp))
+        context.log.debug(f'Total of records returned {len(resp)}')
         for item in resp:
             if isinstance(item, ldapasn1_impacket.SearchResultEntry) is not True:
                 continue
@@ -78,18 +69,18 @@ class CMEModule:
                             trust_transitive.append('PAM Trust')
                         if not trust_transitive:
                             trust_transitive.append('Other')
-                trust_transitive = ', '.join(trust_transitive) 
+                trust_transitive = ', '.join(trust_transitive)
 
-                if flat_name != '' and trust_partner != '' and trust_direction != '' and trust_transitive != '':
-                    trusts.append((flat_name, trust_partner, trust_direction, trust_transitive)) 
+                if flat_name and trust_partner and trust_direction and trust_transitive:
+                    trusts.append((flat_name, trust_partner, trust_direction, trust_transitive))
             except Exception as e:
-                context.log.debug('Cannot process trust relationship due to error %s' % str(e))
+                context.log.debug(f'Cannot process trust relationship due to error {e}')
                 pass
 
-        if len(trusts) > 0:
+        if trusts:
             context.log.success('Found the following trust relationships:')
             for trust in trusts:
-                context.log.highlight('{} -> {} -> {}'.format(trust[1], trust[2], trust[3]))
+                context.log.highlight(f'{trust[1]} -> {trust[2]} -> {trust[3]}')
         else:
             context.log.info('No trust relationships found')
 
